16175e99535be94614361397e5fc91bed58b43a7
comojin1994 <comojin1994@gmail.com>
2020-08-13 20:30:10 +0900

    Fix model of mobilenet v1

--- posenet/models/mobilenet_v1.py
+++ posenet/models/mobilenet_v1.py
@@ -17,23 +17,26 @@ class MobileNetV1(tf.keras.Model):
         )
         self.features.trainable = False
         self.conv = Conv2D(1024, 1, padding='same')
-        self.dconv = Conv2DTranspose(1024, 3, strides=2, padding='same')
-
+        self.dconv1 = Conv2DTranspose(1024, 3, strides=2, padding='same')
+        self.dconv2 = Conv2DTranspose(1024, 3, strides=2, padding='same')
+        
         self.heatmap = Conv2D(17, 1, 1, activation='sigmoid', padding='same')
         self.offset = Conv2D(34, 1, 1 , padding='same')
-        self.displacement_fwd = Conv2D(32, 1, 1, padding='same')
-        self.displacement_bwd = Conv2D(32, 1, 1, padding='same')
+        # self.displacement_fwd = Conv2D(32, 1, 1, padding='same')
+        # self.displacement_bwd = Conv2D(32, 1, 1, padding='same')
 
     def call(self, x):
         x = self.features(x)
         x = self.conv(x)
-        x = self.dconv(x)
+        x = self.dconv1(x)
+        x = self.dconv2(x)
 
         heatmap = self.heatmap(x)
         offset = self.offset(x)
-        displacement_fwd = self.displacement_fwd(x)
-        displacement_bwd = self.displacement_bwd(x)
-        return heatmap, offset, displacement_fwd, displacement_bwd
+        # displacement_fwd = self.displacement_fwd(x)
+        # displacement_bwd = self.displacement_bwd(x)
+        # return heatmap, offset, displacement_fwd, displacement_bwd
+        return heatmap, offset
 
 if __name__ == '__main__':
     loss_object = tf.keras.losses.MeanSquaredError()
