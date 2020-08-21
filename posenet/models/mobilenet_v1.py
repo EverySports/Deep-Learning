fc3ae72d507ba2a8fa50608c21ca9c8e6c80fa11
comojin1994 <comojin1994@gmail.com>
2020-08-21 17:31:27 +0900

    Fix mobilenet v1

--- posenet/models/mobilenet_v1.py
+++ posenet/models/mobilenet_v1.py
@@ -2,6 +2,45 @@ import tensorflow as tf
 
 from tensorflow.keras.applications import MobileNet
 from tensorflow.keras.layers import *
+from tensorflow.python.keras import backend
+
+class DepthwiseConvBlock(tf.keras.Model):
+    def __init__(self, pointwise_conv_filters, alpha=1.0, depth_multiplier=1,
+                          strides=(1, 1), block_id=1):
+        super(DepthwiseConvBlock, self).__init__()
+        self.channel_axis = 1 if backend.image_data_format() == 'channels_first' else -1
+        self.pointwise_conv_filters = int(pointwise_conv_filters * alpha)
+        self.depth_multiplier = depth_multiplier
+        self.strides = strides
+        self.block_id = block_id
+
+        self.depth_conv = DepthwiseConv2D((3, 3),
+                                   padding='same' if self.strides == (1, 1) else 'valid',
+                                   depth_multiplier=self.depth_multiplier,
+                                   strides=self.strides,
+                                   use_bias=False,
+                                   name='conv_dw_%d' % self.block_id)
+        self.bn1 = BatchNormalization(
+            axis=self.channel_axis, name='conv_dw_%d_bn' % self.block_id)
+        self.relu1 = ReLU(6., name='conv_dw_%d_relu' % self.block_id)
+        self.conv = Conv2D(
+            self.pointwise_conv_filters, (1, 1),
+            padding='same',
+            use_bias=False,
+            strides=(1, 1),
+            name='conv_pw_%d' % self.block_id)
+        self.bn2 = BatchNormalization(
+            axis=self.channel_axis, name='conv_pw_%d_bn' % self.block_id)
+        self.relu2 = ReLU(6., name='conv_pw_%d_relu' % self.block_id)
+
+    def call(self, x):
+        x = self.depth_conv(x)
+        x = self.bn1(x)
+        x = self.relu1(x)
+        x = self.conv(x)
+        x = self.bn2(x)
+        x = self.relu2(x)
+        return x
 
 class MobileNetV1(tf.keras.Model):
     def __init__(self, model_id='mobilenet_v1', input_shape=(256, 256, 3), output_stride=16):
@@ -16,22 +55,22 @@ class MobileNetV1(tf.keras.Model):
             classifier_activation=None,
         )
         # self.features.trainable = False
-        self.conv1 = Conv2D(1024, 1, padding='same')
+        self.conv1 = DepthwiseConvBlock(1024)
         self.dconv1 = Conv2DTranspose(1024, 3, strides=2, padding='same')
         self.bn1 = BatchNormalization()
-        self.conv2 = Conv2D(1024, 3, 1, padding='same')
-        self.conv3 = Conv2D(1024, 3, 1, padding='same')
+        self.conv2 = DepthwiseConvBlock(1024)
+        self.conv3 = DepthwiseConvBlock(1024)
         self.dconv2 = Conv2DTranspose(1024, 3, strides=2, padding='same')
         self.bn2 = BatchNormalization()
-        self.conv4 = Conv2D(1024, 3, 1, padding='same')
-        self.conv5 = Conv2D(1024, 3, 1, padding='same')
+        self.conv4 = DepthwiseConvBlock(1024)
+        self.conv5 = DepthwiseConvBlock(1024)
 
-        self.heatmap_conv1 = Conv2D(512, 3, 1, padding='same')
-        self.heatmap_conv2 = Conv2D(256, 3, 1, padding='same')
+        self.heatmap_conv1 = DepthwiseConvBlock(512)
+        self.heatmap_conv2 = DepthwiseConvBlock(256)
         self.heatmap = Conv2D(17, 3, 1, activation='sigmoid', padding='same')
 
-        self.offset_conv1 = Conv2D(512, 3, 1, padding='same')
-        self.offset_conv2 = Conv2D(256, 3, 1, padding='same')
+        self.offset_conv1 = DepthwiseConvBlock(512)
+        self.offset_conv2 = DepthwiseConvBlock(256)
         self.offset = Conv2D(34, 3, 1 , padding='same')
         # self.displacement_fwd = Conv2D(32, 1, 1, padding='same')
         # self.displacement_bwd = Conv2D(32, 1, 1, padding='same')
@@ -40,33 +79,33 @@ class MobileNetV1(tf.keras.Model):
         x = self.features(x)
         x = self.conv1(x)
 
-        x = tf.nn.relu(x)
+        x = tf.nn.swish(x)
         x = self.dconv1(x)
         x = self.bn1(x)
-        x = tf.nn.relu(x)
+        x = tf.nn.swish(x)
         x = self.conv2(x)
-        x = tf.nn.relu(x)
+        x = tf.nn.swish(x)
         x = self.conv3(x)
 
         x = tf.nn.relu(x)
         x = self.dconv2(x)
         x = self.bn2(x)
-        x = tf.nn.relu(x)
+        x = tf.nn.swish(x)
         x = self.conv4(x)
-        x = tf.nn.relu(x)
+        x = tf.nn.swish(x)
         x = self.conv5(x)
-        x = tf.nn.relu(x)
+        x = tf.nn.swish(x)
 
         heatmap = self.heatmap_conv1(x)
-        heatmap = tf.nn.relu(heatmap)
+        heatmap = tf.nn.swish(heatmap)
         heatmap = self.heatmap_conv2(heatmap)
-        heatmap = tf.nn.relu(heatmap)
+        heatmap = tf.nn.swish(heatmap)
         heatmap = self.heatmap(heatmap)
 
         offset = self.offset_conv1(x)
-        offset = tf.nn.relu(offset)
+        offset = tf.nn.swish(offset)
         offset = self.offset_conv2(offset)
-        offset = tf.nn.relu(offset)
+        offset = tf.nn.swish(offset)
         offset = self.offset(offset)
         # displacement_fwd = self.displacement_fwd(x)
         # displacement_bwd = self.displacement_bwd(x)
