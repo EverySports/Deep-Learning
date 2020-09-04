b5df9e2eee24cdbb12b641504d4c6a3a39cbf363
comojin1994 <comojin1994@gmail.com>
2020-09-04 17:06:55 +0900

    ༼ つ ◕_◕ ༽つ Fix config

--- config.py
+++ config.py
@@ -1,14 +1,25 @@
 import tensorflow as tf
+import tensorflow_addons as tfa
 
 learning_rate = 1e-4
-optimizer = tf.keras.optimizers.Adam(learning_rate)
+lr_schedule = tfa.optimizers.CyclicalLearningRate(
+            initial_learning_rate=1e-4,
+            maximal_learning_rate=1e-2,
+            step_size=2000,
+            scale_fn=lambda x: 1.,
+            scale_mode="cycle",
+            name="MyCyclicScheduler")
+# optimizer = tf.keras.optimizers.Adam(learning_rate)
+# optimizer = tf.keras.optimizers.RMSprop(learning_rate)
+optimizer = tf.keras.optimizers.SGD(learning_rate=lr_schedule)
 
 batch_size = 8
 mode = 'mobilenet_v1'
-EPOCHS = 1
+EPOCHS = 100
 
 alpha = .90
 gamma = 2
-loss_weight = [1000, 1]
+# loss_weight = [1000, 1]
+loss_weight = 1000
 
-ckpt_path = '20200816_more_deep_layer'
+ckpt_path = '20200901_EfficientPose-focal_alpha-90'
